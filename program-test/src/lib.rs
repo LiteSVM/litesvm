3e9a7da5e7e5cff1b4f13dfecb0dc2c9430eb198
Jean Marchand <jeanno11@orange.fr>
2023-12-12 08:55:50 +0100

    Add spl programs in program test

--- program-test/src/lib.rs
+++ program-test/src/lib.rs
@@ -1,4 +1,5 @@
 use light_sol_bankrun::bank::LightBank;
+use solana_sdk::pubkey;
 use solana_sdk::{account::Account, pubkey::Pubkey};
 use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
@@ -8,6 +9,39 @@ pub struct ProgramTest {
 }
 
 impl ProgramTest {
+    pub fn new() -> Self {
+        let program_test = Self {
+            bank: Arc::new(RwLock::new(LightBank::new())),
+        };
+        program_test.load_spl_programs();
+        program_test
+    }
+
+    pub fn load_spl_programs(&self) {
+        let mut bank = self.get_bank_mut();
+
+        bank.store_program(
+            pubkey!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
+            include_bytes!("programs/spl_token-3.5.0.so"),
+        );
+        bank.store_program(
+            pubkey!("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"),
+            include_bytes!("programs/spl_token_2022-0.9.0.so"),
+        );
+        bank.store_program(
+            pubkey!("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo"),
+            include_bytes!("programs/spl_memo-1.0.0.so"),
+        );
+        bank.store_program(
+            pubkey!("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
+            include_bytes!("programs/spl_memo-3.0.0.so"),
+        );
+        bank.store_program(
+            pubkey!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"),
+            include_bytes!("programs/spl_associated_token_account-1.1.1.so"),
+        );
+    }
+
     pub fn get_bank(&self) -> RwLockReadGuard<'_, LightBank> {
         self.bank.read().unwrap()
     }
@@ -24,6 +58,9 @@ impl ProgramTest {
         self.get_bank_mut().get_account(pubkey)
     }
 
+    pub fn store_program(&self, program_id: Pubkey, program_bytes: &[u8]) {
+        self.get_bank_mut().store_program(program_id, program_bytes)
+    }
     // pub fn deploy_program(
     //     &self,
     //     bank: &mut LightBank,
