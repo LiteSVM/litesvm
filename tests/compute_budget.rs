e381947a4ee73b1a6c05dcb6aa67003bbdda9ef5
Kevin Heavey <heaveykevin2@gmail.com>
2024-03-26 01:47:39 +0400

    add test_set_compute_unit_limit

---
+++ tests/compute_budget.rs
@@ -0,0 +1,41 @@
+use litesvm::LiteSVM;
+use solana_program::{
+    instruction::InstructionError, message::Message, pubkey::Pubkey, system_instruction::transfer,
+};
+use solana_sdk::{
+    compute_budget::ComputeBudgetInstruction,
+    signature::Keypair,
+    signer::Signer,
+    transaction::{Transaction, TransactionError},
+};
+
+#[test]
+fn test_set_compute_unit_limit() {
+    // see that the tx fails if we set a tiny limit
+    let from_keypair = Keypair::new();
+    let from = from_keypair.pubkey();
+    let to = Pubkey::new_unique();
+
+    let mut bank = LiteSVM::new();
+
+    bank.airdrop(&from, 100).unwrap();
+
+    let instruction = transfer(&from, &to, 64);
+    let tx = Transaction::new(
+        &[&from_keypair],
+        Message::new(
+            &[
+                ComputeBudgetInstruction::set_compute_unit_limit(10),
+                instruction,
+            ],
+            Some(&from),
+        ),
+        bank.latest_blockhash(),
+    );
+    let tx_res = bank.send_transaction(tx);
+
+    assert_eq!(
+        tx_res.unwrap_err().err,
+        TransactionError::InstructionError(0, InstructionError::ComputationalBudgetExceeded)
+    );
+}
