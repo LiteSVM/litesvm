cc5711f4fbadc7b9d78192338935998bba3e6338
kevinheavey <heaveykevin2@gmail.com>
2024-03-28 17:34:15 +0400

    make max_perf benchmark faster

--- benches/max_perf.rs
+++ benches/max_perf.rs
@@ -25,13 +25,12 @@ fn make_tx(
     payer_pk: &Pubkey,
     blockhash: solana_program::hash::Hash,
     payer_kp: &Keypair,
-    deduper: u8,
 ) -> Transaction {
     let msg = Message::new_with_blockhash(
         &[Instruction {
             program_id,
             accounts: vec![AccountMeta::new(counter_address, false)],
-            data: vec![0, deduper],
+            data: vec![0, 0],
         }],
         Some(payer_pk),
         &blockhash,
@@ -50,21 +49,19 @@ fn criterion_benchmark(c: &mut Criterion) {
     svm.store_program(program_id, &read_counter_program());
     svm.airdrop(&payer_pk, 1000000000).unwrap();
     let counter_address = Pubkey::new_unique();
+    let latest_blockhash = svm.latest_blockhash();
+    let tx = make_tx(
+        program_id,
+        counter_address,
+        &payer_pk,
+        latest_blockhash,
+        &payer_kp,
+    );
     c.bench_function("max_perf", |b| {
         b.iter(|| {
             let _ = svm.set_account(counter_address, counter_acc(program_id));
-            svm.expire_blockhash();
-            let latest_blockhash = svm.latest_blockhash();
-            for deduper in 0..NUM_GREETINGS {
-                let tx = make_tx(
-                    program_id,
-                    counter_address,
-                    &payer_pk,
-                    latest_blockhash,
-                    &payer_kp,
-                    deduper,
-                );
-                svm.send_transaction(tx).unwrap();
+            for _ in 0..NUM_GREETINGS {
+                svm.send_transaction(tx.clone()).unwrap();
             }
             assert_eq!(
                 svm.get_account(&counter_address).unwrap().data[0],
