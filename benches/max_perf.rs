028b908cfda8a8cd3995078a69bab77288932eab
Kevin Heavey <heaveykevin2@gmail.com>
2024-03-29 00:17:33 +0400

    Allow optionally skipping sigverify (#45)
    * allow optionally skipping sigverify
    
    * add max_perf benchmark
    
    * make max_perf benchmark faster
    
    * add flamegraph.sh

---
+++ benches/max_perf.rs
@@ -0,0 +1,84 @@
+use std::path::PathBuf;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use litesvm::LiteSVM;
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+};
+use solana_sdk::{
+    account::Account, message::Message, signature::Keypair, signer::Signer,
+    transaction::Transaction,
+};
+
+fn read_counter_program() -> Vec<u8> {
+    let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    so_path.push("test_programs/target/deploy/counter.so");
+    std::fs::read(so_path).unwrap()
+}
+
+const NUM_GREETINGS: u8 = 255;
+
+fn make_tx(
+    program_id: Pubkey,
+    counter_address: Pubkey,
+    payer_pk: &Pubkey,
+    blockhash: solana_program::hash::Hash,
+    payer_kp: &Keypair,
+) -> Transaction {
+    let msg = Message::new_with_blockhash(
+        &[Instruction {
+            program_id,
+            accounts: vec![AccountMeta::new(counter_address, false)],
+            data: vec![0, 0],
+        }],
+        Some(payer_pk),
+        &blockhash,
+    );
+    Transaction::new(&[payer_kp], msg, blockhash)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut svm = LiteSVM::new()
+        .with_sigverify(false)
+        .with_transaction_history(0);
+    let payer_kp = Keypair::new();
+    let payer_pk = payer_kp.pubkey();
+    let program_id = Pubkey::new_unique();
+
+    svm.add_program(program_id, &read_counter_program());
+    svm.airdrop(&payer_pk, 1000000000).unwrap();
+    let counter_address = Pubkey::new_unique();
+    let latest_blockhash = svm.latest_blockhash();
+    let tx = make_tx(
+        program_id,
+        counter_address,
+        &payer_pk,
+        latest_blockhash,
+        &payer_kp,
+    );
+    c.bench_function("max_perf", |b| {
+        b.iter(|| {
+            let _ = svm.set_account(counter_address, counter_acc(program_id));
+            for _ in 0..NUM_GREETINGS {
+                svm.send_transaction(tx.clone()).unwrap();
+            }
+            assert_eq!(
+                svm.get_account(&counter_address).unwrap().data[0],
+                NUM_GREETINGS
+            );
+        })
+    });
+}
+
+fn counter_acc(program_id: Pubkey) -> solana_sdk::account::Account {
+    Account {
+        lamports: 5,
+        data: vec![0_u8; std::mem::size_of::<u32>()],
+        owner: program_id,
+        ..Default::default()
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
