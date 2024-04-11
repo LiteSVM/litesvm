e0c281eafcb8ddeac14467bd2d0389a5670599e7
kevinheavey <heaveykevin2@gmail.com>
2024-04-11 19:03:57 +0400

    update max_perf benchmark to disable blockhash check

--- benches/max_perf.rs
+++ benches/max_perf.rs
@@ -34,6 +34,7 @@ fn make_tx(
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut svm = LiteSVM::new()
+        .with_blockhash_check(false)
         .with_sigverify(false)
         .with_transaction_history(0);
     let payer_kp = Keypair::new();
