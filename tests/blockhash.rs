1c8675b07b4fd6deaad31256065fab035b7fb56f
kevinheavey <heaveykevin2@gmail.com>
2024-04-11 17:26:10 +0400

    add durable nonce test

--- tests/blockhash.rs
+++ tests/blockhash.rs
@@ -1,9 +1,12 @@
 use litesvm::LiteSVM;
 use solana_program::{message::Message, pubkey::Pubkey, system_instruction::transfer};
 use solana_sdk::{
+    account::ReadableAccount,
+    nonce::State as NonceState,
     rent::Rent,
     signature::Keypair,
     signer::Signer,
+    system_instruction::advance_nonce_account,
     transaction::{Transaction, TransactionError},
 };
 
@@ -26,4 +29,40 @@ fn test_invalid_blockhash() {
     let tx_res = svm.send_transaction(tx);
 
     assert_eq!(tx_res.unwrap_err().err, TransactionError::BlockhashNotFound);
+}
+
+#[test_log::test]
+fn test_durable_nonce() {
+    let from_keypair = Keypair::new();
+    let from = from_keypair.pubkey();
+    let to = Pubkey::new_unique();
+    let nonce_kp = Keypair::new();
+
+    let mut svm = LiteSVM::new();
+
+    svm.airdrop(&from, 1_000_000_000).unwrap();
+    let create_nonce_ixns = solana_program::system_instruction::create_nonce_account(
+        &from,
+        &nonce_kp.pubkey(),
+        &from,
+        1_500_000,
+    );
+    let tx = Transaction::new(
+        &[&from_keypair, &nonce_kp],
+        Message::new_with_blockhash(&create_nonce_ixns, Some(&from), &svm.latest_blockhash()),
+        svm.latest_blockhash(),
+    );
+    svm.send_transaction(tx).unwrap();
+    let nonce_account_raw = svm.get_account(&nonce_kp.pubkey()).unwrap();
+    let transfer_ix = transfer(&from, &to, 1);
+    let advance_ix = advance_nonce_account(&nonce_kp.pubkey(), &from);
+    let parsed: NonceState = bincode::deserialize(nonce_account_raw.data()).unwrap();
+    if let NonceState::Initialized(data) = parsed {
+        let nonce = data.blockhash();
+        let msg = Message::new_with_blockhash(&[advance_ix, transfer_ix], Some(&from), &nonce);
+        let tx_using_nonce = Transaction::new(&[&from_keypair], msg, nonce);
+        svm.send_transaction(tx_using_nonce).unwrap();
+    } else {
+        panic!("Uninitialized nonce.")
+    }
 }
