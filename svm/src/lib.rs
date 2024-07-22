df15bc1c930cb0a68e98b50d1f2b388933102be7
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2024-07-22 17:24:50 -0500

    feat: add ed25519/secp256k1 precompiles

--- svm/src/lib.rs
+++ svm/src/lib.rs
@@ -21,6 +21,7 @@ use solana_sdk::{
     account::{Account, AccountSharedData, ReadableAccount, WritableAccount},
     bpf_loader,
     clock::Clock,
+    ed25519_program,
     epoch_rewards::EpochRewards,
     epoch_schedule::EpochSchedule,
     feature_set::{
@@ -38,6 +39,7 @@ use solana_sdk::{
     pubkey::Pubkey,
     rent::Rent,
     reserved_account_keys::ReservedAccountKeys,
+    secp256k1_program,
     signature::{Keypair, Signature},
     signer::Signer,
     slot_hashes::SlotHashes,
@@ -473,6 +475,13 @@ impl LiteSVM {
                 let mut account_found = true;
                 let account = if solana_sdk::sysvar::instructions::check_id(key) {
                     construct_instructions_account(message)
+                } else if ed25519_program::check_id(key) || secp256k1_program::check_id(key) {
+                    let mut account = AccountSharedData::default();
+                    account.set_owner(native_loader::id());
+                    account.set_lamports(1);
+                    account.set_executable(true);
+
+                    account
                 } else {
                     let instruction_account = u8::try_from(i)
                         .map(|i| instruction_accounts.contains(&&i))
