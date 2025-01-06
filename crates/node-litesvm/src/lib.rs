9acae7edabc0c27c1a46a93c8ce1a6b17ee937e0
kevinheavey <heaveykevin2@gmail.com>
2025-01-06 17:21:29 +0400

    more napi rough work

--- crates/node-litesvm/src/lib.rs
+++ crates/node-litesvm/src/lib.rs
@@ -1,9 +1,23 @@
 #![deny(clippy::all)]
 use {
-    litesvm::{error::LiteSVMError, LiteSVM as LiteSVMOriginal},
+    litesvm::{
+        error::LiteSVMError,
+        types::{
+            FailedTransactionMetadata as FailedTransactionMetadataOriginal,
+            SimulatedTransactionInfo, TransactionMetadata as TransactionMetadataOriginal,
+            TransactionResult,
+        },
+        LiteSVM as LiteSVMOriginal,
+    },
     napi::bindgen_prelude::*,
     solana_compute_budget::compute_budget::ComputeBudget,
-    solana_sdk::{account::Account as AccountOriginal, pubkey::Pubkey, signature::Signature, transaction::VersionedTransaction},
+    solana_sdk::{
+        account::Account as AccountOriginal, feature_set::FeatureSet,
+        inner_instruction::InnerInstruction as InnerInstructionOriginal,
+        instruction::CompiledInstruction as CompiledInstructionOriginal, pubkey::Pubkey,
+        signature::Signature, transaction::VersionedTransaction,
+    },
+    std::path::Path,
 };
 
 #[macro_use]
@@ -16,6 +30,62 @@ fn convert_pubkey(address: Uint8Array) -> Pubkey {
 fn to_js_error(e: LiteSVMError, msg: &str) -> Error {
     Error::new(Status::GenericFailure, format!("{msg}: {e}"))
 }
+
+#[derive(Debug, Clone)]
+#[napi]
+pub struct CompiledInstruction(CompiledInstructionOriginal);
+
+#[napi]
+impl CompiledInstruction {
+    #[napi(constructor)]
+    pub fn new(program_id_index: u8, accounts: Uint8Array, data: Uint8Array) -> Self {
+        Self(CompiledInstructionOriginal {
+            program_id_index,
+            accounts: accounts.to_vec(),
+            data: data.to_vec(),
+        })
+    }
+
+    #[napi(getter)]
+    pub fn program_id_index(&self) -> u8 {
+        self.0.program_id_index
+    }
+
+    #[napi(getter)]
+    pub fn accounts(&self) -> Uint8Array {
+        Uint8Array::new(self.0.accounts.clone())
+    }
+
+    #[napi(getter)]
+    pub fn data(&self) -> Uint8Array {
+        Uint8Array::new(self.0.data.clone())
+    }
+}
+
+#[derive(Debug, Clone)]
+#[napi]
+pub struct InnerInstruction(InnerInstructionOriginal);
+
+#[napi]
+impl InnerInstruction {
+    #[napi(getter)]
+    pub fn instruction(&self) -> CompiledInstruction {
+        CompiledInstruction(self.0.instruction.clone())
+    }
+
+    #[napi(getter)]
+    pub fn stack_height(&self) -> u8 {
+        self.0.stack_height
+    }
+}
+
+#[derive(Debug, Clone)]
+#[napi]
+pub struct TransactionMetadata(TransactionMetadataOriginal);
+
+#[derive(Debug, Clone)]
+#[napi]
+pub struct FailedTransactionMetadata(FailedTransactionMetadataOriginal);
 
 #[derive(Debug, Clone)]
 #[napi]
@@ -84,47 +154,48 @@ impl LiteSVM {
     }
 
     /// Sets the compute budget.
-    pub fn with_compute_budget(mut self, compute_budget: ComputeBudget) -> Self {
+    pub fn with_compute_budget(self, compute_budget: ComputeBudget) -> Self {
         Self(self.0.with_compute_budget(compute_budget.into()))
     }
     /// Enables or disables sigverify
-    pub fn with_sigverify(mut self, sigverify: bool) -> Self {
+    pub fn with_sigverify(self, sigverify: bool) -> Self {
         Self(self.0.with_sigverify(sigverify))
     }
     /// Enables or disables the blockhash check
-    pub fn with_blockhash_check(mut self, check: bool) -> Self {
+    pub fn with_blockhash_check(self, check: bool) -> Self {
         Self(self.0.with_blockhash_check(check))
     }
     /// Includes the default sysvars
-    pub fn with_sysvars(mut self) -> Self {
+    pub fn with_sysvars(self) -> Self {
         Self(self.0.with_sysvars())
     }
 
     /// Changes the default builtins
-    pub fn with_builtins(mut self, feature_set: Option<FeatureSet>) -> Self {
+    pub fn with_builtins(self, feature_set: Option<FeatureSet>) -> Self {
         Self(self.0.with_builtins(feature_set.map(|x| x.into())))
     }
 
     /// Changes the initial lamports in LiteSVM's airdrop account
-    pub fn with_lamports(mut self, lamports: u64) -> Self {
+    pub fn with_lamports(self, lamports: u64) -> Self {
         Self(self.0.with_lamports(lamports))
     }
 
     /// Includes the standard SPL programs
-    pub fn with_spl_programs(mut self) -> Self {
+    pub fn with_spl_programs(self) -> Self {
         Self(self.0.with_spl_programs())
     }
 
     /// Changes the capacity of the transaction history.
     /// Set this to 0 to disable transaction history and allow duplicate transactions.
-    pub fn with_transaction_history(mut self, capacity: usize) -> Self {
+    pub fn with_transaction_history(self, capacity: usize) -> Self {
         Self(self.0.with_transaction_history(capacity))
     }
 
-    pub fn with_log_bytes_limit(mut self, limit: Option<usize>) -> Self {
+    pub fn with_log_bytes_limit(self, limit: Option<usize>) -> Self {
         Self(self.0.with_log_bytes_limit(limit))
     }
-    pub fn with_precompiles(mut self, feature_set: Option<FeatureSet>) -> Self {
+
+    pub fn with_precompiles(self, feature_set: Option<FeatureSet>) -> Self {
         Self(self.0.with_precompiles(feature_set.map(|x| x.into())))
     }
 
@@ -138,16 +209,21 @@ impl LiteSVM {
         self.0.get_account(&convert_pubkey(pubkey)).map(Account)
     }
 
+    #[napi]
     /// Sets all information associated with the account of the provided pubkey.
-    pub fn set_account(&mut self, pubkey: Uint8Array, data: Account) -> Result<()> {
-        self.0.set_account(convert_pubkey(pubkey), data.into())
+    pub fn set_account(&mut self, pubkey: Uint8Array, data: &Account) -> Result<()> {
+        self.0
+            .set_account(convert_pubkey(pubkey), data.0.clone())
+            .map_err(|e| to_js_error(e, "Failed to set account"))
     }
 
+    #[napi]
     /// Gets the balance of the provided account pubkey.
     pub fn get_balance(&self, pubkey: Uint8Array) -> Option<u64> {
         self.0.get_balance(&convert_pubkey(pubkey))
     }
 
+    #[napi]
     /// Gets the latest blockhash.
     pub fn latest_blockhash(&self) -> String {
         self.0.latest_blockhash().to_string()
@@ -172,23 +248,39 @@ impl LiteSVM {
     ) -> Result<()> {
         self.0
             .add_program_from_file(convert_pubkey(program_id), path)
+            .map_err(|e| {
+                Error::new(
+                    Status::GenericFailure,
+                    format!("Failed to add program: {e}"),
+                )
+            })
     }
 
     /// Adds am SBF program to the test environment.
     pub fn add_program(&mut self, program_id: Uint8Array, program_bytes: &[u8]) {
-        self.0.add_program(convert_pubkey(program_id), program_bytes)
+        self.0
+            .add_program(convert_pubkey(program_id), program_bytes)
     }
 
     /// Submits a transaction.
     pub fn send_transaction(&mut self, tx: impl Into<VersionedTransaction>) -> Result<()> {
-        self.0.send_transaction(tx)
+        let res = self.0.send_transaction(tx).map_err(|e| {
+            Error::new(
+                Status::GenericFailure,
+                format!("Failed to send transaction: {e:?}"),
+            )
+        })?;
+        Ok(())
     }
     /// Simulates a transaction.
-    pub fn simulate_transaction(
-        &self,
-        tx: impl Into<VersionedTransaction>,
-    ) -> Result<SimulatedTransactionInfo, FailedTransactionMetadata> {
-        self.0.simulate_transaction(tx)
+    pub fn simulate_transaction(&self, tx: impl Into<VersionedTransaction>) -> Result<()> {
+        let res = self.0.simulate_transaction(tx).map_err(|e| {
+            Error::new(
+                Status::GenericFailure,
+                format!("Failed to simulate transaction: {e:?}"),
+            )
+        })?;
+        Ok(())
     }
 
     /// Expires the current blockhash
