ad5bfeff9c9ebdf67bddf86378da7eccbc9943b7
kevinheavey <heaveykevin2@gmail.com>
2025-01-13 00:34:33 +0400

    fix clock test

--- crates/node-litesvm/tests/clock.test.ts
+++ crates/node-litesvm/tests/clock.test.ts
@@ -1,19 +1,27 @@
-import { Clock, start } from "litesvm";
 import {
+	Clock,
+	FailedTransactionMetadata,
+	LiteSVM,
+	TransactionMetadata,
+} from "litesvm";
+import {
+	Keypair,
+	LAMPORTS_PER_SOL,
 	PublicKey,
 	Transaction,
 	TransactionInstruction,
 } from "@solana/web3.js";
 
-test("clock", async () => {
+test("clock", () => {
 	const programId = PublicKey.unique();
-	const context = await start(
-		[{ name: "bankrun_clock_example", programId }],
-		[],
+	const svm = new LiteSVM();
+	svm.addProgramFromFile(
+		programId,
+		"../litesvm/test_programs/target/deploy/litesvm_clock_example.so",
 	);
-	const client = context.banksClient;
-	const payer = context.payer;
-	const blockhash = context.lastBlockhash;
+	const payer = new Keypair();
+	svm.airdrop(payer.publicKey, BigInt(LAMPORTS_PER_SOL));
+	const blockhash = svm.latestBlockhash();
 	const ixs = [
 		new TransactionInstruction({ keys: [], programId, data: Buffer.from("") }),
 	];
@@ -21,21 +29,21 @@ test("clock", async () => {
 	tx.recentBlockhash = blockhash;
 	tx.add(...ixs);
 	tx.sign(payer);
-	// this will fail because it's not January 1970 anymore
-	await expect(client.processTransaction(tx)).rejects.toThrow(
-		"Program failed to complete",
-	);
+	// set the time to January 1st 2000
+	const initialClock = svm.getClock();
+	initialClock.unixTimestamp = 1735689600;
+	svm.setClock(initialClock);
+	// this will fail because the contract wants it to be January 1970
+	const failed = svm.sendTransaction(tx);
+	if (failed instanceof FailedTransactionMetadata) {
+		expect(failed.err().toString()).toContain("ProgramFailedToComplete");
+	} else {
+		throw new Error("Expected transaction failure here");
+	}
 	// so let's turn back time
-	const currentClock = await client.getClock();
-	context.setClock(
-		new Clock(
-			currentClock.slot,
-			currentClock.epochStartTimestamp,
-			currentClock.epoch,
-			currentClock.leaderScheduleEpoch,
-			50n,
-		),
-	);
+	const newClock = svm.getClock();
+	newClock.unixTimestamp = 50;
+	svm.setClock(newClock);
 	const ixs2 = [
 		new TransactionInstruction({
 			keys: [],
@@ -48,5 +56,6 @@ test("clock", async () => {
 	tx2.add(...ixs2);
 	tx2.sign(payer);
 	// now the transaction goes through
-	await client.processTransaction(tx2);
+	const success = svm.sendTransaction(tx2);
+	expect(success).toBeInstanceOf(TransactionMetadata);
 });
