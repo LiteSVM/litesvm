d3b2c2f534397aca7d6b9dc552de34a1535f7c33
Kevin Heavey <heaveykevin2@gmail.com>
2025-03-22 13:10:20 +0000

    add test for problematic program

---
+++ crates/litesvm/tests/test_dflow_load.rs
@@ -0,0 +1,13 @@
+use {litesvm::LiteSVM, solana_pubkey::pubkey};
+
+// https://github.com/LiteSVM/litesvm/issues/140
+#[test]
+fn test_dflow_load() {
+    let mut svm = LiteSVM::new();
+    let program_bytes =
+        include_bytes!("../test_programs/DF1ow3DqMj3HvTj8i8J9yM2hE9hCrLLXpdbaKZu4ZPnz.so");
+    svm.add_program(
+        pubkey!("DF1ow3DqMj3HvTj8i8J9yM2hE9hCrLLXpdbaKZu4ZPnz"),
+        program_bytes,
+    );
+}
