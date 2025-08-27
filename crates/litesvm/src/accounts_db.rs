74a99466bcfc1197c42bd1f89e060e7e009f71fe
Jean (Exotic Markets) <6496148+Aursen@users.noreply.github.com>
2025-08-27 14:19:04 +0700

    Allow access to the internal accounts db (#205)
    * Allow access to the internal accounts db
    
    * Make all the fields public
    
    ---------
    
    Co-authored-by: Aursen <aursen@users.noreply.github.com>

--- crates/litesvm/src/accounts_db.rs
+++ crates/litesvm/src/accounts_db.rs
@@ -62,14 +62,14 @@ where
 }
 
 #[derive(Clone, Default)]
-pub(crate) struct AccountsDb {
-    inner: HashMap<Pubkey, AccountSharedData>,
-    pub(crate) programs_cache: ProgramCacheForTxBatch,
-    pub(crate) sysvar_cache: SysvarCache,
+pub struct AccountsDb {
+    pub inner: HashMap<Pubkey, AccountSharedData>,
+    pub programs_cache: ProgramCacheForTxBatch,
+    pub sysvar_cache: SysvarCache,
 }
 
 impl AccountsDb {
-    pub(crate) fn get_account(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
+    pub fn get_account(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
         self.inner.get(pubkey).map(|acc| acc.to_owned())
     }
 
