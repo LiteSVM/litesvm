e915259bd05210315737abeb38c39459d4c1648c
Aursen <6496148+Aursen@users.noreply.github.com>
2025-09-26 08:07:49 +0700

    Add pubkey_signer test (#222)
    * Add pubkey_signer test
    
    * fix fmt
    
    * Fix clippy
    
    ---------
    
    Co-authored-by: Aursen <aursen@users.noreply.github.com>

---
+++ crates/litesvm/tests/pubkey_signer.rs
@@ -0,0 +1,33 @@
+use {
+    agave_feature_set::FeatureSet, litesvm::LiteSVM, solana_message::Message,
+    solana_native_token::LAMPORTS_PER_SOL, solana_pubkey::Pubkey, solana_signature::Signature,
+    solana_system_interface::instruction::transfer, solana_transaction::Transaction,
+};
+
+#[test]
+fn pubkey_signer() {
+    let mut svm = LiteSVM::default()
+        .with_feature_set(FeatureSet::all_enabled())
+        .with_builtins()
+        .with_lamports(1_000_000u64.wrapping_mul(LAMPORTS_PER_SOL))
+        .with_sysvars()
+        .with_precompiles()
+        .with_default_programs()
+        .with_sigverify(false)
+        .with_blockhash_check(true);
+
+    let dean = Pubkey::new_unique();
+    svm.airdrop(&dean, 10 * LAMPORTS_PER_SOL).unwrap();
+    let jacob = Pubkey::new_unique();
+
+    let ix = transfer(&dean, &jacob, LAMPORTS_PER_SOL);
+    let hash = svm.latest_blockhash();
+    let tx = Transaction {
+        message: Message::new_with_blockhash(&[ix], Some(&dean), &hash),
+        signatures: vec![Signature::default()],
+    };
+    svm.send_transaction(tx).unwrap();
+
+    assert!(svm.get_balance(&dean).unwrap() < 9 * LAMPORTS_PER_SOL);
+    assert_eq!(svm.get_balance(&jacob).unwrap(), LAMPORTS_PER_SOL);
+}
