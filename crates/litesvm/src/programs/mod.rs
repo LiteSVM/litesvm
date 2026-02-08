9e3bbd1a83ca714fbc4909d09fd876740ba399b7
kevinheavey <heaveykevin2@gmail.com>
2026-02-08 22:29:18 +0400

    avoid double-loading trusted default programs

--- crates/litesvm/src/programs/mod.rs
+++ crates/litesvm/src/programs/mod.rs
@@ -5,39 +5,39 @@ use {
 };
 
 pub fn load_default_programs(svm: &mut LiteSVM) {
-    svm.add_program(
+    svm.add_program_preverified(
         address!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
         include_bytes!("elf/spl_token-3.5.0.so"),
     )
     .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address!("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"),
         include_bytes!("elf/spl_token_2022-10.0.0.so"),
     )
     .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address!("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo"),
         include_bytes!("elf/spl_memo-1.0.0.so"),
     )
     .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address!("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
         include_bytes!("elf/spl_memo-3.0.0.so"),
     )
     .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"),
         include_bytes!("elf/spl_associated_token_account-1.1.1.so"),
     )
     .unwrap();
-    svm.add_program(config::ID, include_bytes!("elf/config.so"))
+    svm.add_program_preverified(config::ID, include_bytes!("elf/config.so"))
         .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address_lookup_table::ID,
         include_bytes!("elf/address_lookup_table.so"),
     )
     .unwrap();
-    svm.add_program(
+    svm.add_program_preverified(
         address!("Stake11111111111111111111111111111111111111"),
         include_bytes!("elf/core_bpf_stake-1.0.1.so"),
     )
