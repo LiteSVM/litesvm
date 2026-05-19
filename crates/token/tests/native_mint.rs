6c11796cf6861be175d9d7bfadc0c161f04f5bba
Aryan Khilari <ksharyan18@gmail.com>
2026-05-19 17:57:36 +0530

    test: added coverage for_token_program_id (#341)

--- crates/token/tests/native_mint.rs
+++ crates/token/tests/native_mint.rs
@@ -38,3 +38,36 @@ fn test() {
 
     SyncNative::new(svm, &payer_kp, &account_pk).send().unwrap();
 }
+
+#[test]
+fn test_token_program_id() {
+    let svm = &mut LiteSVM::new();
+
+    let payer_kp = Keypair::new();
+    let payer_pk = payer_kp.pubkey();
+
+    svm.airdrop(&payer_pk, LAMPORTS_PER_SOL * 10).unwrap();
+
+    CreateNativeMint::new(svm, &payer_kp)
+        .token_program_id(&spl_token_2022_interface::ID)
+        .send()
+        .unwrap();
+
+    let mint: Mint = get_spl_account(svm, &spl_token_2022_interface::native_mint::ID).unwrap();
+
+    assert_eq!(mint.decimals, 9);
+    assert_eq!(mint.supply, 0);
+    assert!(mint.mint_authority.is_none());
+    assert!(mint.is_initialized);
+    assert_eq!(mint.freeze_authority, None.into());
+
+    let account_pk = CreateAssociatedTokenAccount::new(
+        svm,
+        &payer_kp,
+        &spl_token_2022_interface::native_mint::ID,
+    )
+    .send()
+    .unwrap();
+
+    SyncNative::new(svm, &payer_kp, &account_pk).send().unwrap();
+}
