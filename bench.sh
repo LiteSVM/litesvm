5b78fef3b1f856a32b52afcf1da931862dc9e9e5
Aursen <6496148+Aursen@users.noreply.github.com>
2026-08-20 09:28:15 +0700

    chore: move bench.sh and flamegraph.sh into justfile

--- bench.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-ROOT=$(git rev-parse --show-toplevel)
-
-cd $ROOT/svm/test_programs
-cargo build-sbf
-
-cd $ROOT
-RUST_LOG= cargo bench --features internal-test
--- flamegraph.sh
+++
@@ -1,3 +0,0 @@
-# See this if the last part (where it says "perf record")
-# is slow https://github.com/flamegraph-rs/flamegraph/issues/74#issuecomment-1909417039
-CARGO_PROFILE_RELEASE_DEBUG=true cargo flamegraph --bench max_perf -- --bench
